5545d3e083dfe1681c0751e70c2187d1b826dde5
UMAR MUHAMMAD <umarmuhdfata@gmail.com>
2026-05-10 16:47:51 -0700

    Initial commit

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,4 +1,27 @@
+// server/auth.js
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 const SECRET_KEY = process.env.JWT_SECRET;
+
+// Generate JWT token
+function generateToken(user) {
+  return jwt.sign(
+    { id: user.id, email: user.email },
+    SECRET_KEY,
+    { expiresIn: '1h' }   // token zai ƙare bayan awa ɗaya
+  );
+}
+
+// Hash password
+async function hashPassword(password) {
+  const saltRounds = 10;
+  return await bcrypt.hash(password, saltRounds);
+}
+
+// Compare password
+async function comparePassword(password, hash) {
+  return await bcrypt.compare(password, hash);
+}
+
+module.exports = { generateToken, hashPassword, comparePassword };
