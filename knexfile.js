85d290a20bc08f9e95af10e4fe111a799af9583f
UMAR MUHAMMAD <umarmuhdfata@gmail.com>
2026-05-12 22:53:46 -0700

    Add users table migration

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      directory: './migrations'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      directory: './migrations'
+    }
+  }
+};
